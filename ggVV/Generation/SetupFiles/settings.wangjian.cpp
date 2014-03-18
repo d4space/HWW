f12a932491ca0a74dc6179f3ad348a888a41f866
SangEun Lee <d4space@gmail.com>
2014-03-19 00:21:47 +0100

    hahahaha

---
+++ ggVV/Generation/SetupFiles/settings.wangjian.cpp
@@ -0,0 +1,140 @@
+#include <cmath>
+using std::log;
+using std::sqrt;
+#include <cstdlib>
+using std::getenv;
+
+#include "settings.h"
+#include "phasespace.h"
+#include "amp_formcalc.h"
+#include "pdf.h"
+#include "pi.h"
+#include "small_powers.h"
+#include "rng.h"
+#include "utilities.h"
+
+// Description =============================================================
+
+const string settingsDescription = "LHC Higgs Cross Section WG, arXiv:1101.0593 [hep-ph], App. A, G_\\mu scheme";
+
+// MC INTEGRATION MODE =====================================================
+
+int maxNumberOfGridAdaptationIterations = 5;     // zero: skip grid adaptation
+const Int64 gridAdaptationNumberOfShots = 50*numberOfEvents;
+const Int64 mainRunNumberOfShots = 100*numberOfEvents;
+long int seedForRandomNumberGenerator = 1;
+
+// EVENT GENERATION MODE ===================================================
+
+const bool generateEvents = true;
+const bool unweightEvents = true;
+const int numberOfEvents = 1000;
+
+// STANDARD MODEL PARAMETERS ===============================================
+
+// input parameters
+const double SMP::gfermi = 1.16637e-5;
+const double SMP::mW = 80.398;
+const double SMP::mZ = 91.1876;
+// derived parameters
+const double SMP::sin2w = 1. - pow2(SMP::mW/SMP::mZ);
+const double SMP::alpha = sqrt(2.)/Pi*SMP::gfermi*pow2(SMP::mW)*SMP::sin2w;
+// W and Z widths
+const double SMP::wW = 2.08872;   // NLO
+const double SMP::wZ = 2.49595;   // NLO
+// unstable lepton masses (for event generation only)
+const double SMP::mmuon = 0.1057;
+const double SMP::mtau = 1.777;
+// massive quark loop amplitude parameters
+const double SMP::mt = 172.5;
+const double SMP::mb = 4.75;
+// Higgs mass and width
+const double SMP::mH = 126.;
+const double SMP::wH = SMP::gamma_H_HTO();
+// Higgs rescaling relative to SM couplings 
+const double BSM::HggCouplingRescalingFactor = 1.;
+const double BSM::HWWCouplingRescalingFactor = 1.;
+const double BSM::HZZCouplingRescalingFactor = 1.;
+
+// process dependent vector boson mass and width
+#if defined PROCMACRO_WW2l2v
+const double SMP::mV = SMP::mW;
+const double SMP::wV = SMP::wW;
+#endif
+#if defined PROCMACRO_ZAZA2l2l
+const double SMP::mV = SMP::mZ;
+const double SMP::wV = SMP::wZ;
+#endif
+#if defined PROCMACRO_ZAZ_2l2v
+const double SMP::mV = SMP::mZ;
+const double SMP::wV = SMP::wZ;
+#endif
+#if defined PROCMACRO_ZAZA4l
+const double SMP::mV = SMP::mZ;
+const double SMP::wV = SMP::wZ;
+#endif
+#if defined PROCMACRO_WWZAZ_2l2v
+const double SMP::mV = SMP::mW;
+const double SMP::wV = SMP::wW;
+// not defined because of V=W,Z ambiguity
+#endif
+
+// AMPLITUDE ===============================================================
+
+const unsigned int amplitude_selection = (
++AmpSqr::CONTINUUMBACKGROUND
++AmpSqr::HIGGSSIGNAL
+);
+
+// SCALES ==================================================================
+
+vector<Scales*> scales_ptr_vector;
+
+namespace {
+RootshatScales central_scale_1(.5);
+//FixedScales central_scale_1(SMP::mH/2.);
+//FixedScales central_scale_1(SMP::mV);
+}     // unnamed namespace
+
+void initialize_scales()
+{
+  scales_ptr_vector.push_back(&central_scale_1);
+}
+
+const int scale_variation_factor = 2;
+
+// SELECTION CUTS ==========================================================
+
+const double minInvMassPhotonProp = 4.;     // GeV
+
+namespace { const MinimalCutsSelection _selection; }
+//namespace { const GeneratorCutsSelection _selection; }
+//namespace { const StandardCutsSelection _selection; }
+//namespace { const HiggsSearchSelection _selection; }
+
+const Selection& selection = _selection;
+
+// COLLIDER ================================================================
+
+//ConstValue<double> PhaseSpace::E_CMS = 7.e3;     // GeV
+ConstValue<double> PhaseSpace::E_CMS = 8.e3;     // GeV
+//ConstValue<double> PhaseSpace::E_CMS = 13.e3;     // GeV
+//ConstValue<double> PhaseSpace::E_CMS = 14.e3;     // GeV
+
+// PARTON DISTRIBUTION FUNCTIONS and ALPHAS ================================
+
+// select PDF set:
+const string lhapdf_file = "MSTW2008lo68cl.LHgrid";
+//const string lhapdf_file = "MSTW2008nnlo68cl.LHgrid";
+//const string lhapdf_file = "CT10nnlo.LHgrid";    // LHAPDF >= v5.8.8
+//const string lhapdf_file = "NNPDF21_nnlo_100.LHgrid";
+
+// calculate PDF error (if supported by PDF set):
+const bool requestPDFError = false;
+
+// using set's default alpha_s (LHAPDF)
+
+// =========================================================================
+
+// Some settings should only be modified by advanced users:
+#include "advanced_settings.inc"
--- ggVV/Generation/setenv4gg2VV.csh
+++ ggVV/Generation/setenv4gg2VV.csh
@@ -1,7 +1,7 @@
 #!/bin/tcsh -f
 setenv gg2VV_PATH /afs/cern.ch/work/s/salee/private/HWWwidth/CMSSW_5_3_7/src/HWW/ggVV/Generation
 #setenv gg2VV_PATH /afs/cern.ch/work/a/asakharo/gg2VV
-setenv TOPDIR $gg2VV_PATH/gg2VV/gg2VV-3.1.5
+setenv TOPDIR $gg2VV_PATH/gg2VV-3.1.5
 setenv LOOPTOOLS_TOPDIR $gg2VV_PATH/LoopTools-2.8
 source /afs/cern.ch/sw/IntelSoftware/linux/all-setup.csh
 setenv PATH ${PATH}:${gg2VV_PATH}/omniORB-4.1.6/bin
