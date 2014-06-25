9ad32496b79e278d231431daecb88beefad61ff2
DongHyunKim <donghyun.kim.linus@gmail.com>
2014-06-25 21:14:57 +0200

    Nevt=25000*160

--- GGVvAnalyzer/setEnvSourceMe.sh
+++ GGVvAnalyzer/setEnvSourceMe.sh
@@ -1,3 +1,5 @@
 #!/bin/bash 
 #export PATH=${PATH}:/afs/cern.ch/work/s/salee/private/HWWwidth/CMSSW_5_3_7/src/HWW/GGVvAnalyzer/ExRootAnalysis
-export PATH=${PATH}:/terranova_0/HWWwidth/HWW/GGVvAnalyzer/ExRootAnalysis
+source /afs/cern.ch/sw/lcg/external/gcc/4.7/x86_64-slc5/setup.sh
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.18/x86_64-slc5-gcc47-opt/root/bin/thisroot.sh
+export PATH=${PATH}:/afs/cern.ch/work/d/dhkim/private/WORK/HWW/GGVvAnalyzer/ExRootAnalysis
