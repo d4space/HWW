82ff6e4f9798671f04a54c6eb6b758817a301c13
Khakim <khakimjan@yahoo.com>
2014-08-12 22:31:47 +0200

    hahaha

--- GGVvAnalyzer/include/TreeHWWgg2vv8TeV/SetupTreeHWWgg2vv8TeV.h
+++ GGVvAnalyzer/include/TreeHWWgg2vv8TeV/SetupTreeHWWgg2vv8TeV.h
@@ -5,15 +5,26 @@
 #include "chain_SigHw25gg2vv8TeV.h" 
 #include "chain_IntHw25gg2vv8TeV.h" 
 #include "chain_CotHw25gg2vv8TeV.h" 
+#include "chain_SigHw1gg2vv8TeVOnPeak.h" 
+#include "chain_SigHw1gg2vv8TeVShoulder.h" 
+#include "chain_SigHw1gg2vv8TeVTail.h" 
+#include "chain_IntHw1gg2vv8TeVOnPeak.h" 
+#include "chain_IntHw1gg2vv8TeVShoulder.h" 
+#include "chain_IntHw1gg2vv8TeVTail.h" 
 #include "chain_SigHw25gg2vv8TeVOnPeak.h" 
 #include "chain_SigHw25gg2vv8TeVShoulder.h" 
 #include "chain_SigHw25gg2vv8TeVTail.h" 
+#include "chain_IntHw25gg2vv8TeVOnPeak.h" 
+#include "chain_IntHw25gg2vv8TeVShoulder.h" 
+#include "chain_IntHw25gg2vv8TeVTail.h" 
+#include "chain_CotHw25gg2vv8TeVHead.h" 
+#include "chain_CotHw25gg2vv8TeVTail.h" 
 
 void SetupTreeHWWgg2vv8TeV(TString sample, TChain* chains )
 {
   cout<<"SetupTreeHWWgg2vv8TeV.h: "<<sample<<endl;
   //=========
-  //LowPU
+  //gg2vv 8TeV
   //=========
   if( sample == "Hw1Sig8TeV"){
     chain_SigHw1gg2vv8TeV(chains);
@@ -27,12 +38,34 @@ void SetupTreeHWWgg2vv8TeV(TString sample, TChain* chains )
     chain_IntHw25gg2vv8TeV(chains);
   }else if( sample == "Hw25Cot8TeV"){
     chain_CotHw25gg2vv8TeV(chains);
+  }else if(sample == "Hw1Sig8TeVOnPeak"){
+    chain_SigHw1gg2vv8TeVOnPeak(chains);
+  }else if(sample == "Hw1Sig8TeVShoulder"){
+    chain_SigHw1gg2vv8TeVShoulder(chains);
+  }else if(sample == "Hw1Sig8TeVTail"){
+    chain_SigHw1gg2vv8TeVTail(chains);
+  }else if(sample == "Hw1Int8TeVOnPeak"){
+    chain_IntHw1gg2vv8TeVOnPeak(chains);
+  }else if(sample == "Hw1Int8TeVShoulder"){
+    chain_IntHw1gg2vv8TeVShoulder(chains);
+  }else if(sample == "Hw1Int8TeVTail"){
+    chain_IntHw1gg2vv8TeVTail(chains);
   }else if(sample == "Hw25Sig8TeVOnPeak"){
     chain_SigHw25gg2vv8TeVOnPeak(chains);
   }else if(sample == "Hw25Sig8TeVShoulder"){
     chain_SigHw25gg2vv8TeVShoulder(chains);
   }else if(sample == "Hw25Sig8TeVTail"){
     chain_SigHw25gg2vv8TeVTail(chains);
+  }else if(sample == "Hw25Int8TeVOnPeak"){
+    chain_IntHw25gg2vv8TeVOnPeak(chains);
+  }else if(sample == "Hw25Int8TeVShoulder"){
+    chain_IntHw25gg2vv8TeVShoulder(chains);
+  }else if(sample == "Hw25Int8TeVTail"){
+    chain_IntHw25gg2vv8TeVTail(chains);
+  }else if(sample == "Hw25Cot8TeVHead"){
+    chain_CotHw25gg2vv8TeVHead(chains);
+  }else if(sample == "Hw25Cot8TeVTail"){
+    chain_CotHw25gg2vv8TeVTail(chains);
   }else
     cout<<"[SetupTreeHw1gg2vv8TeV.h] Nanigorae? "<<endl;
 }
--- Utils/const.h
+++ Utils/const.h
@@ -1,5 +1,6 @@
 #define PI  3.14159265358979323846
 #define LumiTotal8TeV 19.468 // fb^-1
+#define nMllBin 11
 namespace GenType
 {
   enum{
@@ -23,6 +24,7 @@ namespace GenType
   };
   double M_ele	= 0.000511;
   double M_mu	= 0.1057;
+  double M_tau	= 1.7768;
   double M_W	= 80.385;
   double M_Z	= 91.1876;
 }
