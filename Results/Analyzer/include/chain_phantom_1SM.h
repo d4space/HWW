d2970002965142da1fdce06f57a7d8c0780830b4
SangEun Lee <d4space@gmail.com>
2015-03-17 17:52:58 +0100

    hahahah

--- Results/Analyzer/include/chain_phantom_1SM.h
+++ Results/Analyzer/include/chain_phantom_1SM.h
@@ -1,7 +1,7 @@
 void chain_phantom_1SM( TChain *chains)
 {
   chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_160_qqww1smEM_baseW.root");
-  chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_169_qqww1smTM_baseW.root");
-  chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_172_qqww1smTE_baseW.root");
-  chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_176_qqww1smTT_baseW.root");
+  //chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_169_qqww1smTM_baseW.root");
+  //chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_172_qqww1smTE_baseW.root");
+  //chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_176_qqww1smTT_baseW.root");
 }
--- Results/Analyzer/include/chain_phantom_9SM.h
+++ Results/Analyzer/include/chain_phantom_9SM.h
@@ -1,7 +1,7 @@
 void chain_phantom_9SM( TChain *chains)
 {
   chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_161_qqww9smEM_baseW.root");
-  chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_175_qqww9smTM_baseW.root");
-  chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_173_qqww9smTE_baseW.root");
-  chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_170_qqww9smTT_baseW.root");
+  //chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_175_qqww9smTM_baseW.root");
+  //chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_173_qqww9smTE_baseW.root");
+  //chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/tree_skim_all_hwidth/nominals/latino_170_qqww9smTT_baseW.root");
 }
