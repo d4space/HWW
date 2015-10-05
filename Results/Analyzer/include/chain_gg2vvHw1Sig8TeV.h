b9813e7e64a340fdc1afb03e7b6248dfe096650f
SangEun Lee <d4space@gmail.com>
2015-10-06 01:57:14 +0900

    hahahaha

--- Results/Analyzer/include/chain_gg2vvHw1Sig8TeV.h
+++ Results/Analyzer/include/chain_gg2vvHw1Sig8TeV.h
@@ -3,7 +3,12 @@ void chain_gg2vvHw1Sig8TeV( TChain *chains)
   //chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/gg2vv/latinogg2vv_Hw1_SigOnPeak_8TeV.root");
   //chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/gg2vv/latinogg2vv_Hw1_SigShoulder_8TeV.root");
   //chains->Add("root://eoscms.cern.ch//eos/cms/store/user/kbutanov/HWWwidth/LatinosTrees/gg2vv/latinogg2vv_Hw1_SigTail_8TeV.root");
-  chains->Add("root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/cmshww/amassiro/HiggsWidth/gg2vv/latinogg2vv_Hw1_SigOnPeak_8TeV.root");
-  chains->Add("root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/cmshww/amassiro/HiggsWidth/gg2vv/latinogg2vv_Hw1_SigShoulder_8TeV.root");
-  chains->Add("root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/cmshww/amassiro/HiggsWidth/gg2vv/latinogg2vv_Hw1_SigTail_8TeV.root");
+
+  //chains->Add("root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/cmshww/amassiro/HiggsWidth/gg2vv/latinogg2vv_Hw1_SigOnPeak_8TeV.root");
+  //chains->Add("root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/cmshww/amassiro/HiggsWidth/gg2vv/latinogg2vv_Hw1_SigShoulder_8TeV.root");
+  //chains->Add("root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/cmshww/amassiro/HiggsWidth/gg2vv/latinogg2vv_Hw1_SigTail_8TeV.root");
+
+  chains->Add("/d3/scratch/khakim/LatinosTrees/tree_skim_all_hwidth/nominals/latinogg2vv_Hw1_SigOnPeak_8TeV.root");
+  chains->Add("/d3/scratch/khakim/LatinosTrees/tree_skim_all_hwidth/nominals/latinogg2vv_Hw1_SigShoulder_8TeV.root");
+  chains->Add("/d3/scratch/khakim/LatinosTrees/tree_skim_all_hwidth/nominals/latinogg2vv_Hw1_SigTail_8TeV.root");
 }
